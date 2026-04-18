ec41ee431a5da5c25eac23f9e4bc9dfff86f7890
DomRadomski <domradomski123@gmail.com>
2026-04-18 10:24:16 +0100

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,54 @@ import './App.css'
 
 function App() {
 
+  let [score, setScore] = useState(0)
+  let [bestscore, setBestscore] = useState(0)
+
+  const handleClick = () => {
+    console.log(`${p.name}, ${p.clicked}`); p.clicked = true;
+  }
+
+  return (
+    <div className='memory-game'>
+      <Header score={score} bestscore={bestscore}/>
+      <Pokegrid handleClick={handleClick}/>
+    </div>
+  )
+}
+
+export default App
+
+function Header({ score, bestscore }) {
+  return (
+    <div className='header'>
+      <button>Rules</button>
+      <h1>Pokemon Memory Game</h1>
+      <Counter score={score} bestscore={bestscore}/>
+    </div>
+  )
+}
+
+function Counter({ score, bestscore }) {
+  return (
+    <div className='score'>
+      <span>Current Score: {score} </span>
+      <span>Best Score: {bestscore} </span>
+    </div>
+  )
+}
+
+function Pokegrid({ handleClick }) {
+
   let [pokemon, setPokemon] = useState([
-    {name: 'chimchar', imgurl: null},
-    {name: 'piplup', imgurl: null},
-    {name: 'pikachu', imgurl: null},
-    {name: 'quagsire', imgurl: null},
-    {name: 'gible', imgurl: null},
-    {name: 'turtwig', imgurl: null},
-    {name: 'magikarp', imgurl: null},
-    {name: 'starly', imgurl: null},
-    {name: 'abra', imgurl: null}
+    {name: 'chimchar', imgurl: null, clicked: false},
+    {name: 'piplup', imgurl: null, clicked: false},
+    {name: 'pikachu', imgurl: null, clicked: false},
+    {name: 'quagsire', imgurl: null, clicked: false},
+    {name: 'gible', imgurl: null, clicked: false},
+    {name: 'turtwig', imgurl: null, clicked: false},
+    {name: 'magikarp', imgurl: null, clicked: false},
+    {name: 'starly', imgurl: null, clicked: false},
+    {name: 'abra', imgurl: null, clicked: false}
   ]) 
 
   useEffect(() => {
@@ -35,17 +73,15 @@ function App() {
   return (
     <div className='pokegrid'>
       {pokemon.map(p => (
-        <Pokemon key={p.name} name={p.name} imgurl={p.imgurl}/>
+        <Pokemon key={p.name} name={p.name} imgurl={p.imgurl} onClick={handleClick}/>
       ))}
     </div>
   )
 }
 
-export default App
-
-function Pokemon({ name, imgurl }) {
+function Pokemon({ name, imgurl, handleClick }) {
   return (
-    <div className='pokemon'>
+    <div className='pokemon' onClick={handleClick}>
       <img src={imgurl}/>
       <h1>{name}</h1>
     </div>
